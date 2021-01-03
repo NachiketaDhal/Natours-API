55c545867a28611582e37ce3337fe71f191d9c7b
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-01-03 16:16:59 +0530

    Update Error Handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/globalErrorHandler');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -15,10 +17,10 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
 
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
@@ -27,5 +29,21 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'error',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`));
+});
+
+// GLOBAL ERROR HANDLING MIDLEWARE
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/globalErrorHandler.js
@@ -0,0 +1,12 @@
+// GLOBAL ERROR HANDLING MIDLEWARE
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
