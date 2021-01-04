cd67d5b4cc35418376cdbf5f031d94ac0cef750c
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-01-04 14:00:07 +0530

    Update Error Handling

--- app.js
+++ app.js
@@ -31,11 +31,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'error',
-  //   message: `Can't find ${req.originalUrl} on this server`,
-  // });
-
   // const err = new Error(`Can't find ${req.originalUrl} on this server`);
   // err.status = 'fail';
   // err.statusCode = 404;
--- controllers/globalErrorHandler.js
+++ controllers/globalErrorHandler.js
@@ -1,3 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational error, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.log('ERROR: ', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 // GLOBAL ERROR HANDLING MIDLEWARE
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
@@ -5,8 +35,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
