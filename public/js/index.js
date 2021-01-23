61d16475d5046006cdd6184eda5bc5b617495741
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-01-23 17:47:02 +0530

    Update payment feature

--- public/js/index.js
+++ public/js/index.js
@@ -154,4 +154,4 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 20);
+if (alertMessage) showAlert('success', alertMessage, 20);
